67d82b00c457e304161d5f65a235d13c25ab929d
Tfragao <fragaot@gmail.com>
2023-01-23 16:41:52 +0100

    Basic exercises (added)

---
+++ ThreeSumDemo.java
@@ -0,0 +1,29 @@
+/* *****************************************************************************
+ *  Name:              Ada Lovelace
+ *  Coursera User ID:  123456
+ *  Last modified:     October 16, 1842
+ **************************************************************************** */
+
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdOut;
+
+public class ThreeSumDemo {
+
+    public static int count(int[] a) {
+        // count triples that sum to 0
+        int n = a.length;
+        int cnt = 0;
+        for (int i = 0; i < n; i++)
+            for (int j = i + 1; j < n; j++)
+                for (int k = j + 1; k < n; k++)
+                    if (a[i] + a[j] + a[k] == 0)
+                        cnt++;
+        return cnt;
+    }
+
+    public static void main(String[] args) {
+        int[] a = In.readInts(args[0]);
+        StdOut.println(count(a));
+
+    }
+}
